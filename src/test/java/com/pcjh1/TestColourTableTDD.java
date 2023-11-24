3cf43c9a682f17eae028649cc995b8b3375f25f2
peter <121365693@umail.ucc.ie>
2023-11-24 16:38:49 +0000

    (test) tests added for ColourTableTDD constructor/setter/getter.

--- src/test/java/com/pcjh1/TestColourTableTDD.java
+++ src/test/java/com/pcjh1/TestColourTableTDD.java
@@ -1,4 +1,13 @@
 package com.pcjh1;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 public class TestColourTableTDD {
+    @Test
+    public void testColourTableTDD() {
+        ColourTableTDD colourTable = new ColourTableTDD(4);
+        colourTable.setPalette(4);
+        assertEquals(4, colourTable.getPalette());
+    }
+
 }
