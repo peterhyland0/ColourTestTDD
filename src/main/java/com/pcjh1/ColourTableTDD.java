ddb7b81a2c5a553bdf527cc6bf663e907592d1b8
peter <121365693@umail.ucc.ie>
2023-11-24 18:06:31 +0000

    (code) code implemented for add method to palette array, exception thrown if beyond max

--- src/main/java/com/pcjh1/ColourTableTDD.java
+++ src/main/java/com/pcjh1/ColourTableTDD.java
@@ -1,13 +1,17 @@
 package com.pcjh1;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ColourTableTDD {
     private int palette;
-
+    private List<int[]> colorTable;
     public ColourTableTDD(int palette) {
         if (!inValidPaletteSize(palette)) {
             throw new IllegalArgumentException("Invalid palette size");
         }
         this.palette = palette;
+        this.colorTable = new ArrayList<>();
     }
 
     public void setPalette(int palette) {
@@ -16,6 +20,21 @@ public class ColourTableTDD {
 
     public int getPalette() {
         return palette;
+    }
+
+    public void add(int red, int green, int blue) {
+
+
+        int[] rgbColor = {red, green, blue};
+        if (colorTable.size() < palette) {
+            colorTable.add(rgbColor);
+        } else {
+            throw new IllegalStateException("Exceeded the capacity of the ColourTable");
+        }
+    }
+
+    public List<int[]> getColorTable() {
+        return colorTable;
     }
 
     public boolean inValidPaletteSize(int palette) {
