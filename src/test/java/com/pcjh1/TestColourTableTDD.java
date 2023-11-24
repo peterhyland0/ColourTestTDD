27f9fea0ded4e5847fa6a5f9a66849874ae2e691
peter <121365693@umail.ucc.ie>
2023-11-24 17:36:18 +0000

    (test) implemented test to check if palette size is invalid.

--- src/test/java/com/pcjh1/TestColourTableTDD.java
+++ src/test/java/com/pcjh1/TestColourTableTDD.java
@@ -1,6 +1,9 @@
 package com.pcjh1;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
 import static org.junit.jupiter.api.Assertions.*;
 public class TestColourTableTDD {
     @Test
@@ -8,6 +11,20 @@ public class TestColourTableTDD {
         ColourTableTDD colourTable = new ColourTableTDD(4);
         colourTable.setPalette(4);
         assertEquals(4, colourTable.getPalette());
+
     }
 
-}
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, 3, -234, 7, 11111})
+    public void testColourTableTDDInvalidPaletteSize(int invalidPaletteSize) {
+        assertThrows(IllegalArgumentException.class, () -> {
+            ColourTableTDD colourTable = new ColourTableTDD(invalidPaletteSize);
+        });
+        // Add more assertions for the expected behavior when an invalid palette size is provided
+    }
+
+
+    }
+
+
