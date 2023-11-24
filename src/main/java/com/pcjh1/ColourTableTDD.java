9a133678f1dc83ca8d517522e794521b3d2d2677
peter <121365693@umail.ucc.ie>
2023-11-24 16:39:27 +0000

    (code) implemented code for ColourTableTDD constructor/setter/getter.

--- src/main/java/com/pcjh1/ColourTableTDD.java
+++ src/main/java/com/pcjh1/ColourTableTDD.java
@@ -1,4 +1,14 @@
 package com.pcjh1;
 
 public class ColourTableTDD {
+    private int palette;
+    public ColourTableTDD(int palette) {
+        this.palette = palette;
+    }
+    public void setPalette(int palette) {
+        this.palette = palette;
+    }
+    public int getPalette() {
+        return palette;
+    }
 }
