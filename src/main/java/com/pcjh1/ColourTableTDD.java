96148d17f743b3ede3869abf737e389d03bcd6c4
peter <121365693@umail.ucc.ie>
2023-11-24 17:37:17 +0000

    (code) implemented code to check for invalid palette size (palette > 1 && palette < 1025 && (palette & (palette - 1)) == 0), with method called in constructor

--- src/main/java/com/pcjh1/ColourTableTDD.java
+++ src/main/java/com/pcjh1/ColourTableTDD.java
@@ -2,13 +2,23 @@ package com.pcjh1;
 
 public class ColourTableTDD {
     private int palette;
+
     public ColourTableTDD(int palette) {
+        if (!inValidPaletteSize(palette)) {
+            throw new IllegalArgumentException("Invalid palette size");
+        }
         this.palette = palette;
     }
+
     public void setPalette(int palette) {
         this.palette = palette;
     }
+
     public int getPalette() {
         return palette;
+    }
+
+    public boolean inValidPaletteSize(int palette) {
+        return palette > 1 && palette < 1025 && (palette & (palette - 1)) == 0;
     }
 }
